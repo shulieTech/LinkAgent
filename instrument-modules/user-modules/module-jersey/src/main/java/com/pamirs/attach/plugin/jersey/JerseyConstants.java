3a0794b483c406aa532208524ba260e2749fa825
jirenhe <jirenhe@shulie.io>
2021-12-02 18:33:15 +0800

    jersey

---
+++ instrument-modules/user-modules/module-jersey/src/main/java/com/pamirs/attach/plugin/jersey/JerseyConstants.java
@@ -0,0 +1,12 @@
+package com.pamirs.attach.plugin.jersey;
+
+import com.pamirs.pradar.MiddlewareType;
+
+/**
+ * @author jirenhe | jirenhe@shulie.io
+ * @since 2021/12/02 3:29 下午
+ */
+public class JerseyConstants {
+    public static final String PLUGIN_NAME = "jersey";
+    public static final int CLIENT_PLUGIN_TYPE = MiddlewareType.TYPE_RPC;;
+}

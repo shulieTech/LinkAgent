4e6da2d74587830d5f1d6213a832cc9a8399cd97
linxingqiang <linxingqiang@pamirs.top>
2022-01-14 16:11:12 +0800

    去除jms

--- instrument-modules/user-modules/module-jms/src/main/java/com/pamirs/attach/plugin/jms/JmsConstants.java
+++
@@ -1,8 +0,0 @@
-package com.pamirs.attach.plugin.jms;
-
-import com.pamirs.pradar.MiddlewareType;
-
-public class JmsConstants {
-    public final static String PLUGIN_NAME = "jms";
-    public final static int PLUGIN_TYPE = MiddlewareType.TYPE_MQ;
-}
--- instrument-modules/user-modules/module-jms/src/main/java/com/pamirs/attach/plugin/jms/util/ApplyFunction.java
+++
@@ -1,5 +0,0 @@
-package com.pamirs.attach.plugin.jms.util;
-
-public interface ApplyFunction<T> {
-    T apply(Object... args) throws Throwable;
-}

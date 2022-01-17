c991fb025e01922e1f722493a6cf36c01a2d6684
linxingqiang <linxingqiang@pamirs.top>
2022-01-17 20:16:47 +0800

    静默开关调整

--- instrument-modules/user-modules/module-pradar-config-fetcher/src/main/java/com/shulie/instrument/module/config/fetcher/config/resolver/http/ClusterTestConfigHttpResolver.java
+++ instrument-modules/user-modules/module-pradar-config-fetcher/src/main/java/com/shulie/instrument/module/config/fetcher/config/resolver/http/ClusterTestConfigHttpResolver.java
@@ -14,15 +14,7 @@
  */
 package com.shulie.instrument.module.config.fetcher.config.resolver.http;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import com.alibaba.fastjson.JSON;
-
 import com.pamirs.pradar.AppNameUtils;
 import com.pamirs.pradar.Pradar;
 import com.pamirs.pradar.PradarSwitcher;
@@ -36,6 +28,13 @@ import com.shulie.instrument.module.config.fetcher.config.event.FIELDS;
 import com.shulie.instrument.module.config.fetcher.config.impl.ClusterTestConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author shiyajian
@@ -167,7 +166,7 @@ public class ClusterTestConfigHttpResolver extends AbstractHttpResolver<ClusterT
             if (map != null && map.get(SILENCE_SWITCH_STATUS) != null) {
                 //全局静默开关
                 String status = (String)map.get(SILENCE_SWITCH_STATUS);
-                clusterTestConfig.setSilenceSwitchOn(!CLOSE.equals(status));
+                clusterTestConfig.setSilenceSwitchOn(CLOSE.equals(status));
             }
 
         } catch (Throwable e) {

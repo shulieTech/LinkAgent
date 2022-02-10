f2f13675c935589faafd4cb1bb616e8e801dbc28
linxingqiang <linxingqiang@pamirs.top>
2022-02-10 20:04:12 +0800

    fix mock无法生效的bug

--- instrument-modules/user-modules/module-pradar-core/src/main/java/com/pamirs/pradar/interceptor/TraceInterceptor.java
+++ instrument-modules/user-modules/module-pradar-core/src/main/java/com/pamirs/pradar/interceptor/TraceInterceptor.java
@@ -247,12 +247,10 @@ abstract class TraceInterceptor extends BaseInterceptor {
         } finally {
             try {
                 beforeLast(advice);
-            } catch (ProcessControlException e) {
-                // mock功能通过此异常中断后续流程
             } catch (PradarException e) {
                 LOGGER.error("TraceInterceptor beforeLast exec err, class:" + this.getClass().getName(), e);
                 throwable = e;
-            }catch (PressureMeasureError e) {
+            } catch (PressureMeasureError e) {
                 LOGGER.error("TraceInterceptor beforeLast exec err, class:" + this.getClass().getName(), e);
                 throwable = e;
             } catch (Throwable t) {

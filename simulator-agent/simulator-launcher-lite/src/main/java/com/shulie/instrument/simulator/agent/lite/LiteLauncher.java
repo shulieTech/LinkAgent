8c56a4e7cc68456b2e89696f28ab05fcc5108298
ocean_wll <wanglinglong@shulie.io>
2022-03-04 18:07:05 +0800

    delay改成0

--- simulator-agent/simulator-launcher-lite/src/main/java/com/shulie/instrument/simulator/agent/lite/LiteLauncher.java
+++ simulator-agent/simulator-launcher-lite/src/main/java/com/shulie/instrument/simulator/agent/lite/LiteLauncher.java
@@ -51,7 +51,7 @@ public class LiteLauncher {
     /**
      * simulator 延迟加载时间
      */
-    private static final Integer SIMULATOR_DELAY = 300;
+    private static final Integer SIMULATOR_DELAY = 0;
 
     /**
      * agentHome地址

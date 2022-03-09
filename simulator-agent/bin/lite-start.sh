a99c9420f1bf5fc06fd1442221e20faf61a887fc
ocean_wll <wanglinglong@shulie.io>
2022-03-09 12:20:32 +0800

    修改启动脚本检查，tools.jar是否存在，不存在可以用户手动指定

--- simulator-agent/bin/lite-start.sh
+++ simulator-agent/bin/lite-start.sh
@@ -1,9 +1,37 @@
 JAVAHOME=$JAVA_HOME
-if [ ! $JAVAHOME ]; then
-  echo "启动失败，未配置JAVA_HOME，请配置后重试！"
-  exit 1
-else
-  echo "环境检查成功，日志请查看../logs/lite.log文件。"
-fi
+toolsPath=$JAVA_HOME/lib/tools.jar
 
-nohup java -Xbootclasspath/a:$JAVA_HOME/lib/tools.jar  -jar ../simulator-launcher-lite.jar >../logs/lite.log 2>&1 &
+function checkJavaHome(){
+	echo "检查JAVA_HOME！"
+  if [ ! $JAVAHOME ]; then
+    echo "启动失败，未配置JAVA_HOME，请配置后重启！"
+    exit 1
+  else
+    echo "JAVA_HOME检查成功！"
+  fi
+  return 0;
+}
+
+checkToolsResult=1
+function checkTools(){
+	echo "检查tools.jar!"
+  if [ ! -f "${toolsPath}" ]; then
+    echo "$toolsPath 不存在，请检查后再重启！"
+    checkToolsResult=1
+  else
+    echo "tools.jar检查成功！"
+    checkToolsResult=0
+  fi
+}
+
+checkJavaHome
+checkTools
+echo $checkToolsResult
+while [ 1 == $checkToolsResult ]; do
+    read -p "请手动指定tools.jar路径: " toolsPath
+    checkTools
+done
+
+echo "环境检查成功，启动探针...，启动日志请查看../logs/lite.log"
+
+nohup java -Xbootclasspath/a:$toolsPath -jar ../simulator-launcher-lite.jar >../logs/lite.log 2>&1 &

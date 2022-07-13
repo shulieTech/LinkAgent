b931a6c0d0112a86d2d9f988da1bccb07a5af783
ocean_wll <wanglinglong@shulie.io>
2022-07-13 11:14:42 +0800

    修复grpc NPE问题，增加兜底策略

--- instrument-modules/user-modules/module-grpc/src/main/java/com/pamirs/attach/plugin/grpc/interceptor/ServerStreamListenerImplHalfClosedInterceptor.java
+++ instrument-modules/user-modules/module-grpc/src/main/java/com/pamirs/attach/plugin/grpc/interceptor/ServerStreamListenerImplHalfClosedInterceptor.java
@@ -4,15 +4,16 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package com.pamirs.attach.plugin.grpc.interceptor;
+
 import com.pamirs.attach.plugin.grpc.GrpcConstants;
 import com.pamirs.pradar.Pradar;
 import com.pamirs.pradar.ResultCode;
@@ -20,6 +21,7 @@ import com.pamirs.pradar.interceptor.SpanRecord;
 import com.pamirs.pradar.interceptor.TraceInterceptorAdaptor;
 import com.shulie.instrument.simulator.api.listener.ext.Advice;
 import com.shulie.instrument.simulator.api.resource.DynamicFieldManager;
+import com.shulie.instrument.simulator.api.util.StringUtil;
 
 import javax.annotation.Resource;
 import java.net.SocketTimeoutException;
@@ -30,7 +32,7 @@ import java.util.Map;
  * @date 2021/6/17 17:46
  * io.grpc.internal.ServerCallImpl$ServerStreamListenerImpl
  */
-public class ServerStreamListenerImplHalfClosedInterceptor extends TraceInterceptorAdaptor{
+public class ServerStreamListenerImplHalfClosedInterceptor extends TraceInterceptorAdaptor {
     @Resource
     protected DynamicFieldManager manager;
 
@@ -71,15 +73,17 @@ public class ServerStreamListenerImplHalfClosedInterceptor extends TraceIntercep
         final Map<String, String> invokeContext = manager.getDynamicField(advice.getTarget(), GrpcConstants.DYNAMIC_FIELD_INVOKE_CONTEXT);
         SpanRecord record = new SpanRecord();
         String rpcId = invokeContext.get(Pradar.getInvokeIdKey());
-        String[] rpcIds = rpcId.split("\\.");
-        String lastRpcId = Integer.valueOf(rpcIds[rpcIds.length -1]) + 1 + "";
-        StringBuilder newRpcId = new StringBuilder();
-        for (int i = 0; i < rpcIds.length - 1 ; i ++ ){
-            newRpcId.append(rpcIds[i]).append(".");
+        if (!StringUtil.isEmpty(rpcId)) {
+            String[] rpcIds = rpcId.split("\\.");
+            String lastRpcId = Integer.valueOf(rpcIds[rpcIds.length - 1]) + 1 + "";
+            StringBuilder newRpcId = new StringBuilder();
+            for (int i = 0; i < rpcIds.length - 1; i++) {
+                newRpcId.append(rpcIds[i]).append(".");
+            }
+            newRpcId.append(lastRpcId);
+            invokeContext.put(Pradar.getInvokeIdKey(), newRpcId.toString());
+            record.setContext(invokeContext);
         }
-        newRpcId.append(lastRpcId);
-        invokeContext.put(Pradar.getInvokeIdKey(), newRpcId.toString());
-        record.setContext(invokeContext);
 
         String method = fullMethodName;
         String service = "";

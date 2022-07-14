5fb26ce462b7e48e548e08f42c03c2c930684b4a
jiangjibo <jiangjibo@shulie.io>
2022-07-14 15:34:20 +0800

    eureka相关请求移除压测标，包括心跳，app，状态等

--- instrument-modules/user-modules/module-feign/src/main/java/com/pamirs/attach/plugin/feign/interceptor/EurekaRestRequestInterceptor.java
+++ instrument-modules/user-modules/module-feign/src/main/java/com/pamirs/attach/plugin/feign/interceptor/EurekaRestRequestInterceptor.java
@@ -1,16 +1,35 @@
 package com.pamirs.attach.plugin.feign.interceptor;
 
+import com.pamirs.pradar.InvokeContext;
 import com.pamirs.pradar.Pradar;
 import com.pamirs.pradar.interceptor.AroundInterceptor;
 import com.shulie.instrument.simulator.api.listener.ext.Advice;
 
 public class EurekaRestRequestInterceptor extends AroundInterceptor {
 
+    private ThreadLocal<InvokeContext> threadLocalContext = new ThreadLocal<InvokeContext>();
+
     @Override
     public void doBefore(Advice advice) throws Throwable {
-        // 如果当前线程存在压测标，要去除掉
-        if(Pradar.isClusterTest()){
+        if (Pradar.isClusterTest()) {
+            threadLocalContext.set(Pradar.getInvokeContext());
             Pradar.clearInvokeContext();
         }
+    }
+
+    @Override
+    public void doAfter(Advice advice) throws Throwable {
+        if (threadLocalContext.get() != null) {
+            Pradar.setInvokeContext(threadLocalContext.get());
+        }
+        threadLocalContext.remove();
+    }
+
+    @Override
+    public void doException(Advice advice) throws Throwable {
+        if (threadLocalContext.get() != null) {
+            Pradar.setInvokeContext(threadLocalContext.get());
+        }
+        threadLocalContext.remove();
     }
 }

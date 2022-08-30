16169e626e7b132a50138d41f9d2278ced3fdd57
浪依 <langyi@zjmeitianhui.com>
2022-08-30 13:19:39 +0800

    同步主分支代码， 优化性能

--- instrument-simulator/instrument-simulator-core/src/main/java/com/shulie/instrument/simulator/core/extension/ExtensionAdviceWrapContainer.java
+++ instrument-simulator/instrument-simulator-core/src/main/java/com/shulie/instrument/simulator/core/extension/ExtensionAdviceWrapContainer.java
@@ -93,12 +93,6 @@ public class ExtensionAdviceWrapContainer extends AdviceListenerWrap {
     }
 
     private AdviceListener wrapAdviceListener() {
-        AdviceListener adviceListener = this.adviceListener;
-        for (AdviceListenerWrapBuilder adviceListenerWrapBuilder : GlobalAdviceWrapBuilders.getAdviceListenerWrapBuilders()) {
-/*
-             adviceListener = adviceListenerWrapBuilder.build(adviceListener);
-*/
-        }
         return adviceListener;
     }
 }

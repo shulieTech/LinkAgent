d5833833b499a480660fc85372e47abf9832dfce
jiangjibo <jiangjibo@shulie.io>
2022-09-16 17:03:20 +0800

    影子资源准备，无流量初始化影子数据源

--- instrument-modules/user-modules/module-pradar-core/src/main/java/com/pamirs/pradar/pressurement/agent/shared/service/EventRouter.java
+++ instrument-modules/user-modules/module-pradar-core/src/main/java/com/pamirs/pradar/pressurement/agent/shared/service/EventRouter.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * See the License for the specific language governing permissions and
@@ -76,11 +76,6 @@ public final class EventRouter {
                     if (event == null) {
                         continue;
                     }
-                    StringBuilder sb = new StringBuilder();
-                    for (PradarEventListener listener : listeners) {
-                        sb.append(listener).append(",");
-                    }
-                    LOGGER.info("execute event {} with listeners: {}", event, sb.toString());
                     for (PradarEventListener listener : listeners) {
                         try {
                             EventResult result = listener.onEvent(event);

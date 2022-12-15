d1a2b25f0628e478704578c71b2ea57d19102204
guann1n9 <guann1n9@gamil.com>
2022-12-15 17:08:06 +0800

    spring-quartz

--- instrument-modules/user-modules/module-shadow-job/src/main/java/com/pamirs/attach/plugin/shadowjob/ShadowJobPlugin.java
+++ instrument-modules/user-modules/module-shadow-job/src/main/java/com/pamirs/attach/plugin/shadowjob/ShadowJobPlugin.java
@@ -247,14 +247,14 @@ public class ShadowJobPlugin extends ModuleLifecycleAdapter implements Extension
         });
 
 
-        //
-//        enhanceTemplate.enhance(this, "org.springframework.scheduling.quartz.QuartzJobBean", new EnhanceCallback() {
-//            @Override
-//            public void doEnhance(InstrumentClass target) {
-//                InstrumentMethod getMethod = target.getDeclaredMethod("execute", "org.quartz.JobExecutionContext");
-//                getMethod.addInterceptor(Listeners.of(QuartzJobBeanExecuteInterceptor.class));
-//            }
-//        });
+
+        enhanceTemplate.enhance(this, "org.springframework.scheduling.quartz.QuartzJobBean", new EnhanceCallback() {
+            @Override
+            public void doEnhance(InstrumentClass target) {
+                InstrumentMethod getMethod = target.getDeclaredMethod("execute", "org.quartz.JobExecutionContext");
+                getMethod.addInterceptor(Listeners.of(QuartzJobBeanExecuteInterceptor.class));
+            }
+        });
 
 
         enhanceTemplate.enhance(this, "org.quartz.JobExecutionContext", new EnhanceCallback() {

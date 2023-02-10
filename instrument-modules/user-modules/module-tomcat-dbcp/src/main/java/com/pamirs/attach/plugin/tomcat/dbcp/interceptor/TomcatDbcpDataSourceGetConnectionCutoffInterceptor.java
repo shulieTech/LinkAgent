e4fb5ddbca9cdc5699d4358916619b5c06db74ce
jiangjibo <jiangjibo@shulie.io>
2023-02-10 14:14:08 +0800

    增加tomcat-dbcp模块

---
+++ instrument-modules/user-modules/module-tomcat-dbcp/src/main/java/com/pamirs/attach/plugin/tomcat/dbcp/interceptor/TomcatDbcpDataSourceGetConnectionCutoffInterceptor.java
@@ -0,0 +1,139 @@
+package com.pamirs.attach.plugin.tomcat.dbcp.interceptor;
+
+import com.pamirs.attach.plugin.tomcat.dbcp.ListenerRegisterStatus;
+import com.pamirs.attach.plugin.tomcat.dbcp.util.DataSourceWrapUtil;
+import com.pamirs.attach.plugin.tomcat.dbcp.util.DbcpMediaDataSource;
+import com.pamirs.pradar.CutOffResult;
+import com.pamirs.pradar.Pradar;
+import com.pamirs.pradar.exception.PressureMeasureError;
+import com.pamirs.pradar.interceptor.CutoffInterceptorAdaptor;
+import com.pamirs.pradar.internal.config.ShadowDatabaseConfig;
+import com.pamirs.pradar.pressurement.ClusterTestUtils;
+import com.pamirs.pradar.pressurement.agent.event.IEvent;
+import com.pamirs.pradar.pressurement.agent.event.impl.ClusterTestSwitchOffEvent;
+import com.pamirs.pradar.pressurement.agent.event.impl.ShadowDataSourceConfigModifyEvent;
+import com.pamirs.pradar.pressurement.agent.listener.EventResult;
+import com.pamirs.pradar.pressurement.agent.listener.PradarEventListener;
+import com.pamirs.pradar.pressurement.agent.shared.service.DataSourceMeta;
+import com.pamirs.pradar.pressurement.agent.shared.service.EventRouter;
+import com.pamirs.pradar.pressurement.datasource.util.DbUrlUtils;
+import com.shulie.instrument.simulator.api.listener.ext.Advice;
+import org.apache.commons.lang.StringUtils;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class TomcatDbcpDataSourceGetConnectionCutoffInterceptor extends CutoffInterceptorAdaptor {
+
+    private final static Logger logger = LoggerFactory.getLogger(TomcatDbcpDataSourceGetConnectionCutoffInterceptor.class.getName());
+
+    @Override
+    public CutOffResult cutoff0(Advice advice) throws Throwable {
+        DataSourceWrapUtil.attachment(advice);
+        Object target = advice.getTarget();
+        addListener();
+        ClusterTestUtils.validateClusterTest();
+
+        BasicDataSource dataSource = (BasicDataSource) target;
+
+        DataSourceMeta<BasicDataSource> dataSourceMeta = new DataSourceMeta<BasicDataSource>(dataSource.getUrl(), dataSource.getUsername(), dataSource);
+        DataSourceWrapUtil.init(dataSourceMeta);
+        Connection connection = null;
+
+        /**
+         * 所有的流量均切换到此逻辑上,防止业务有连接缓存后无法进入
+         * 如果未找到配置情况下则当前流量为压测流量时返回null,非压测流量则执行业务连接池正常逻辑,此种情况可能由于数据源未配置的情况
+         * 如果获取连接出错时如果流量为压测流量则返回null，非压测流量则执行业务连接池正常逻辑
+         */
+        if (DataSourceWrapUtil.pressureDataSources.containsKey(dataSourceMeta)) {
+            DbcpMediaDataSource mediatorDataSource = DataSourceWrapUtil.pressureDataSources.get(dataSourceMeta);
+            if (mediatorDataSource != null) {
+                try {
+                    connection = mediatorDataSource.getConnection();
+                } catch (SQLException e) {
+                    if (Pradar.isClusterTest()) {
+                        throw new PressureMeasureError(e);
+                    }
+                    throw e;
+                }
+            } else {
+                if (!Pradar.isClusterTest()) {
+                    return CutOffResult.passed();
+                }
+            }
+            return CutOffResult.cutoff(connection);
+        } else {
+            if (!Pradar.isClusterTest()) {
+                return CutOffResult.passed();
+            }
+            return CutOffResult.cutoff(null);
+        }
+    }
+
+    private void addListener() {
+        if (!ListenerRegisterStatus.getInstance().init()) {
+            return;
+        }
+        EventRouter.router().addListener(new PradarEventListener() {
+            @Override
+            public EventResult onEvent(IEvent event) {
+                if (!(event instanceof ClusterTestSwitchOffEvent)) {
+                    return EventResult.IGNORE;
+                }
+                //关闭压测数据源
+                DataSourceWrapUtil.destroy();
+                return EventResult.success("tomcat-dbcp-plugin");
+            }
+
+            @Override
+            public int order() {
+                return 27;
+            }
+        }).addListener(new PradarEventListener() {
+            @Override
+            public EventResult onEvent(IEvent event) {
+                if (!(event instanceof ShadowDataSourceConfigModifyEvent)) {
+                    return EventResult.IGNORE;
+                }
+                ShadowDataSourceConfigModifyEvent shadowDataSourceConfigModifyEvent = (ShadowDataSourceConfigModifyEvent) event;
+                Set<ShadowDatabaseConfig> target = shadowDataSourceConfigModifyEvent.getTarget();
+                if (null == target || target.size() == 0) {
+                    return EventResult.IGNORE;
+                }
+                for (ShadowDatabaseConfig config : target) {
+                    Iterator<Map.Entry<DataSourceMeta, DbcpMediaDataSource>> it = DataSourceWrapUtil.pressureDataSources.entrySet().iterator();
+                    while (it.hasNext()) {
+                        Map.Entry<DataSourceMeta, DbcpMediaDataSource> entry = it.next();
+                        if (StringUtils.equalsIgnoreCase(DbUrlUtils.getKey(config.getUrl(), config.getUsername()),
+                                DbUrlUtils.getKey(entry.getKey().getUrl(), entry.getKey().getUsername()))) {
+                            DbcpMediaDataSource value = entry.getValue();
+                            it.remove();
+                            try {
+                                value.close();
+                                if (logger.isInfoEnabled()) {
+                                    logger.info("module-tomcat-dbcp: destroyed shadow table datasource success. url:{} ,username:{}", entry.getKey().getUrl(), entry.getKey().getUsername());
+                                }
+                            } catch (Throwable e) {
+                                logger.error("module-tomcat-dbcp: closed datasource err! target:{}, url:{} username:{}", entry.getKey().getDataSource().hashCode(), entry.getKey().getUrl(), entry.getKey().getUsername(), e);
+                            }
+                            break;
+                        }
+                    }
+
+                }
+                return EventResult.success("module-tomcat-dbcp: destroyed shadow table datasource success.");
+            }
+
+            @Override
+            public int order() {
+                return 28;
+            }
+        });
+    }
+}

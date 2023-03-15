38e387737ec5fcb17dd7a527bd25e0a1746d2f7d
jiangjibo <jiangjibo@shulie.io>
2023-03-15 14:17:06 +0800

    sync模块支持强引用，防止引用对象因为gc回收导致增强失败

--- instrument-modules/user-modules/module-sync-fetch-obj/src/main/java/com/pamirs/attach/plugin/syncfetch/interceptor/SyncObjectAfterFetchInterceptor.java
+++ instrument-modules/user-modules/module-sync-fetch-obj/src/main/java/com/pamirs/attach/plugin/syncfetch/interceptor/SyncObjectAfterFetchInterceptor.java
@@ -18,20 +18,18 @@ import java.util.Vector;
 public class SyncObjectAfterFetchInterceptor extends AdviceListener {
     private static final Logger logger = LoggerFactory.getLogger(SyncObjectAfterFetchInterceptor.class);
 
-    public static final List<String> keys = new Vector<String>();
+    private boolean strongReference;
 
-    private boolean weakReference;
-
-    public SyncObjectAfterFetchInterceptor(boolean weakReference) {
-        this.weakReference = weakReference;
+    public SyncObjectAfterFetchInterceptor(boolean strongReference) {
+        this.strongReference = strongReference;
     }
 
     @Override
     public void after(Advice advice) throws Throwable {
         String key = SyncObjectFetchUtils.getKey(advice.getTargetClass(), advice.getBehaviorName());
-        logger.info("success save sync object after invoked from : {}", key);
+        logger.info("success save sync object after invoked from : {}, reference:{}", key, strongReference ? "strong" : "weak");
         SyncObjectService.saveSyncObject(key, new SyncObject()
-                .addData(new SyncObjectData(advice.getTarget(), advice.getBehaviorName(), advice.getParameterArray(), advice.getBehavior().getParameterTypes(), advice.getReturnObj(), weakReference)));
+                .addData(new SyncObjectData(advice.getTarget(), advice.getBehaviorName(), advice.getParameterArray(), advice.getBehavior().getParameterTypes(), advice.getReturnObj(), strongReference)));
     }
 
 }

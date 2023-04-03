b28ccf0926d3e826f98e8eec787ad0b4186d213d
linxingqiang <linxingqiang@pamirs.top>
2023-04-03 17:04:38 +0800

    gw穿透支持

---
+++ instrument-modules/user-modules/module-servlet-common/src/main/java/com/pamirs/attach/plugin/common/web/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.pamirs.attach.plugin.common.web.utils;
+
+/**
+ * @author angju
+ * @date 2023/4/3 10:29
+ */
+public class Constants {
+    public static final String PRADAR_CLUSTER_FLAG_GW = "pradar_cluster_flag_gw";
+    public static final String extendParamEnd = "pradar_cluster_flag_gw_end";
+
+}

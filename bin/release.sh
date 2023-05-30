09f9628ccfe5a9ccae81028d4cade6589d904fc2
jiangjibo <jiangjibo@shulie.io>
2023-05-30 16:40:28 +0800

    修改打包脚本

--- bin/release.sh
+++ bin/release.sh
@@ -10,7 +10,8 @@ echo "start deploy ..."
 sh ${HOME}/bin/deploy.sh
 echo "end deploy ..."
 
-java -cp ${HOME}/instrument-modules/user-modules/module-pradar-core/target/module-pradar-core-2.0.2.0.jar com.shulie.instrument.module.pradar.core.dependency.PradarCoreModuleDependenciesCollector
+cd ${HOME}/instrument-modules/user-modules/module-pradar-core
+mvn io.shulie.instrument.module:dependency-processor:1.1:DependencyJarCollect
 
 echo "release finish ..."
 echo "release home package: ${HOME}/deploy"

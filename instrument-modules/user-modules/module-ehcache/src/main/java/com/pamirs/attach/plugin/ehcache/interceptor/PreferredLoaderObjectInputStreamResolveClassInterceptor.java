dec8ba01c857dfb1ecad58b3cedac42f90a12974
jiangjibo <jiangjibo@shulie.io>
2023-08-03 10:41:02 +0800

    小版本提升到5.6.6.2

--- instrument-modules/user-modules/module-ehcache/src/main/java/com/pamirs/attach/plugin/ehcache/interceptor/PreferredLoaderObjectInputStreamResolveClassInterceptor.java
+++ instrument-modules/user-modules/module-ehcache/src/main/java/com/pamirs/attach/plugin/ehcache/interceptor/PreferredLoaderObjectInputStreamResolveClassInterceptor.java
@@ -1,22 +1,25 @@
 package com.pamirs.attach.plugin.ehcache.interceptor;
 
 import com.pamirs.attach.plugin.dynamic.reflect.ReflectionUtils;
+import com.pamirs.pradar.CutOffResult;
 import com.pamirs.pradar.cache.ClusterTestCacheWrapperKey;
+import com.pamirs.pradar.interceptor.CutoffInterceptorAdaptor;
 import com.pamirs.pradar.interceptor.ParametersWrapperInterceptorAdaptor;
 import com.shulie.instrument.simulator.api.annotation.ListenerBehavior;
 import com.shulie.instrument.simulator.api.listener.ext.Advice;
 
 @ListenerBehavior(isFilterClusterTest = true)
-public class PreferredLoaderObjectInputStreamResolveClassInterceptor extends ParametersWrapperInterceptorAdaptor {
+public class PreferredLoaderObjectInputStreamResolveClassInterceptor extends CutoffInterceptorAdaptor {
 
     @Override
-    protected Object[] getParameter0(Advice advice) throws Throwable {
+    public CutOffResult cutoff0(Advice advice) throws Throwable {
         Object arg = advice.getParameterArray()[0];
         if (arg instanceof ClusterTestCacheWrapperKey) {
             arg = ((ClusterTestCacheWrapperKey) arg).getKey();
         }
         String className = ReflectionUtils.invoke(arg, "getName");
         ClassLoader loader = ReflectionUtils.get(advice.getTarget(), "loader");
-        return new Object[]{Class.forName(className, false, loader)};
+        return CutOffResult.cutoff(Class.forName(className, false, loader));
     }
+
 }

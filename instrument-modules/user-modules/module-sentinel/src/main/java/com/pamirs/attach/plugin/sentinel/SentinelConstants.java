6aab6d5c3ea94957b1b402fc17604d3111ee0aee
jirenhe <jirenhe@shulie.io>
2023-12-26 15:30:44 +0800

    add sentinel

---
+++ instrument-modules/user-modules/module-sentinel/src/main/java/com/pamirs/attach/plugin/sentinel/SentinelConstants.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2021 Shulie Technology, Co.Ltd
+ * Email: shulie@shulie.io
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pamirs.attach.plugin.sentinel;
+
+import com.pamirs.pradar.MiddlewareType;
+
+/**
+ * @author xiaobin.zfb
+ * @since 2020/8/14 10:06 下午
+ */
+public class SentinelConstants {
+    public final static String PLUGIN_NAME = "sentinel";
+    public final static int PLUGIN_TYPE = MiddlewareType.TYPE_LOCAL;
+
+    public final static String DYNAMIC_FIELD_CONTEXT = "context";
+}
